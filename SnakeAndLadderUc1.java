c10c4188927df9b9b0a9a08e5d3d7b8330100593
Jayesh <jayesh.bhosale1812@gmail.com>
2021-10-17 18:15:09 +0530

    Snake and ladder problem added

---
+++ SnakeAndLadderUc1.java
@@ -0,0 +1,12 @@
+package practise;
+
+public class SnakeAndLadderUc1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder game");
+		int playerPosition=0;
+		System.out.println("Snake and Ladder game statring at position="+playerPosition);
+
+	}
+
+}
